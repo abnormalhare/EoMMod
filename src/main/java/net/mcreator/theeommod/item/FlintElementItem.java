2df541bf718f9da45717f2196ad6c0580425580f
TheWeirdNerdyGamer <weirdnerdygamer20@gmail.com>
2020-11-23 16:35:37 -0500

    fixed creative tabs for items because I'm still a freaking idiot

--- src/main/java/net/mcreator/theeommod/item/FlintElementItem.java
+++ src/main/java/net/mcreator/theeommod/item/FlintElementItem.java
@@ -5,10 +5,10 @@ import net.minecraftforge.registries.ObjectHolder;
 
 import net.minecraft.item.Rarity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.block.BlockState;
 
+import net.mcreator.theeommod.itemgroup.EomModItemGroup;
 import net.mcreator.theeommod.TheEomModModElements;
 
 @TheEomModModElements.ModElement.Tag
@@ -25,7 +25,7 @@ public class FlintElementItem extends TheEomModModElements.ModElement {
 	}
 	public static class ItemCustom extends Item {
 		public ItemCustom() {
-			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			super(new Item.Properties().group(EomModItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON));
 			setRegistryName("flint_element");
 		}
 
