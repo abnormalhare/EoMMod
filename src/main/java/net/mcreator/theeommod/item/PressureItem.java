124abc06e180af0e43b1fe1f5b80a85227045099
TheWeirdNerdyGamer <weirdnerdygamer20@gmail.com>
2020-11-24 09:32:33 -0500

    gravity recipe shapeless aaaaaa im so dumb

--- src/main/java/net/mcreator/theeommod/item/PressureItem.java
+++ src/main/java/net/mcreator/theeommod/item/PressureItem.java
@@ -1,24 +1,29 @@
 
 package net.mcreator.theeommod.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.theeommod.itemgroup.EomModItemGroup;
+import net.mcreator.theeommod.TheEomModModElements;
+
 @TheEomModModElements.ModElement.Tag
 public class PressureItem extends TheEomModModElements.ModElement {
-
 	@ObjectHolder("the_eom_mod:pressure")
 	public static final Item block = null;
-
 	public PressureItem(TheEomModModElements instance) {
 		super(instance, 31);
-
 	}
 
 	@Override
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
-
 	public static class ItemCustom extends Item {
-
 		public ItemCustom() {
 			super(new Item.Properties().group(EomModItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON));
 			setRegistryName("pressure");
@@ -38,7 +43,5 @@ public class PressureItem extends TheEomModModElements.ModElement {
 		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 			return 1F;
 		}
-
 	}
-
 }
