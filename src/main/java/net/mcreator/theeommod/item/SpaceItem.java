161343f93a678c0043576b9f933da0545fb5f892
TheWeirdNerdyGamer <weirdnerdygamer20@gmail.com>
2021-04-12 14:46:30 -0400

    Added Death, Extinction, Nothing, and Space

---
+++ src/main/java/net/mcreator/theeommod/item/SpaceItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.theeommod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.theeommod.TheEomModModElements;
+
+@TheEomModModElements.ModElement.Tag
+public class SpaceItem extends TheEomModModElements.ModElement {
+	@ObjectHolder("the_eom_mod:space")
+	public static final Item block = null;
+	public SpaceItem(TheEomModModElements instance) {
+		super(instance, 69);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("space");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
